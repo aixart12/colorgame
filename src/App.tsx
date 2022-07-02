f36b36ef0d18c263c23d27d810c0ed79f478dfca
Dhruv Kumar <dhruvlol12@gmail.com>
2022-07-02 10:38:55 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryCustomProvider } from "./hoc/QueryCustomContext";
+import AdminRoutes from "./routes/AdminRoutes";
+import UserRoutes from "./routes/UserRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ChakraProvider>
+        <QueryCustomProvider>
+          <Routes>
+            <Route path='/admin' element={<AdminRoutes/>}/>
+            <Route path='/' element={<UserRoutes/>}/>
+          </Routes>
+        </QueryCustomProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
